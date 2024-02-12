913605ca6ba8fff26f6d5b947bb097703c7cbff1
huseyin corakli <huseyincorakli@icloud.com>
2024-02-12 22:20:28 +0300

    S3

---
+++ RabbitMQ.Publisher_S3/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://ymsclgok:bW2XZJGz5gmJSCs7D0NFtcX_RxbHvkR-@sparrow.rmq.cloudamqp.com/ymsclgok");
+
+using IConnection connection = factory.CreateConnection(); //Bağlantı Aktifleştirme
+using IModel channel = connection.CreateModel(); //Kanal açma
+
+//1.adım ==> exchange oluşturup adı ve tipini gir
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj:");
+    string message= Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    //2.adım ilgili exchange'e routkey vererek hangi kuyruğa gideceğini belirt
+    channel.BasicPublish(exchange:"direct-exchange-example",routingKey:"direct-queue-example",body:byteMessage);
+}
+
+
+Console.Read();
