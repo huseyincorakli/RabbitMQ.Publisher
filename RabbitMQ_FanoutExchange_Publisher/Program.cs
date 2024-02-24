4e3098bba293c7a4cd5781c810992844b7b9aecb
huseyin corakli <huseyincorakli@icloud.com>
2024-02-25 02:16:01 +0300

    Fanout exhange

---
+++ RabbitMQ_FanoutExchange_Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://ymsclgok:bW2XZJGz5gmJSCs7D0NFtcX_RxbHvkR-@sparrow.rmq.cloudamqp.com/ymsclgok");
+
+using IConnection connection = factory.CreateConnection(); //Bağlantı Aktifleştirme
+using IModel channel = connection.CreateModel(); //Kanal açma
+
+string exchangeName = "fanout-exchange-example";
+
+channel.ExchangeDeclare(exchange: exchangeName, ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(1000);
+    byte[] message = Encoding.UTF8.GetBytes($"Message {i}");
+    
+    channel.BasicPublish(
+        exchange: exchangeName,
+        routingKey: string.Empty,
+        body: message);
+}
